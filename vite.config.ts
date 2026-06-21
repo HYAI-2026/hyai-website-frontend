725c51f2cf2429cbc5dd500d65b67602f1c3271d
박성철 <parksung-cheol@bagseongcheol-ui-MacBookAir.local>
2026-06-21 17:06:28 +0900

    Deploy GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base : '/hyai-website-frontend/',
   assetsInclude: ['**/*.mov'],
 })
