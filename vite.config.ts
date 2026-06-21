ae4b89f69e1d9acd0f58ff055859770898efa902
박성철 <parksung-cheol@bagseongcheol-ui-MacBookAir.local>
2026-06-21 19:46:44 +0900

    Update site build

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base : '/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
   assetsInclude: ['**/*.mov'],
 })
