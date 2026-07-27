1006a732332b6b91e3055ce60190694d2b0175ed
박성철 <qwmk1025@hanyang.ac.kr>
2026-07-27 19:01:43 +0900

    학회 링크 수정

--- src/components/home/JoinButton.tsx
+++ src/components/home/JoinButton.tsx
@@ -4,7 +4,7 @@ import styles from '../../assets/styles/JoinButton.module.css'
 export default function JoinButton() {
   return (
     <div className={styles.wrapper}>
-      <AppLink href="https://forms.gle/3DexGKEx34vJYFBn8" className={styles.button}>
+      <AppLink href="https://forms.gle/5ukQ6aMDWTxVuM7u5" className={styles.button}>
         가입하기
         <span className={styles.arrow} aria-hidden="true">
           →
